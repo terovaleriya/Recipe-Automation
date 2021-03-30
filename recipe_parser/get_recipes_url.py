7dd80f140c079f394b7ac2a60e404f112ecb704e
Valeriia Terova <terovaleriya@gmail.com>
2021-03-30 16:21:15 +0200

    refactored

--- recipe_parser/get_recipes_url.py
+++ recipe_parser/get_recipes_url.py
@@ -1,3 +1,4 @@
+import os
 import re
 
 from recipe_parser.html_loader import get_html, get_soup
@@ -5,8 +6,10 @@ from recipe_parser.html_loader import get_html, get_soup
 # страница с рецептами Waitrose
 url = "https://www.waitrose.com/content/waitrose/en/home/recipes.html"
 
+os.chdir("files")
+
 # файл, куда будем писать url
-to_file = "recipes_urls_A4.txt"
+to_file = "recipes_urls.txt"
 
 # достаем список категорий
 page_html = get_html(url)
@@ -20,24 +23,26 @@ categories = set(categories)
 
 # из каждой категории достаем все рецепты
 # recipes_per_category_*.txt – распределение рецептов по категориям, url могут повторяться
-#
 all_recipes = []
-distribution = open('recipes_per_category_A4.txt', 'w')
 for category in categories:
-    category_html = get_html(category)
-    category_soup = get_soup(category_html)
-    recipes = category_soup.findAll("a", href=re.compile("/home/recipes/recipe_directory.+$"))
-    distribution.write("\nКатегория: " + category + "\n")
-    distribution.write("Рецептов в категории: " + str(len(recipes)) + "\n")
+    try:
+        with open('recipes_per_category.txt', 'w') as distribution:
+            category_html = get_html(category)
+            category_soup = get_soup(category_html)
+            recipes = category_soup.findAll("a", href=re.compile("/home/recipes/recipe_directory.+$"))
+            distribution.write("\nКатегория: " + category + "\n")
+            distribution.write("Рецептов в категории: " + str(len(recipes)) + "\n")
 
-    recipes = [recipe['href'] for recipe in recipes]
-    recipes = [
-        recipe if recipe.startswith("https://www.waitrose.com") else 'https://www.waitrose.com' + recipe + ".html"
-        for recipe in recipes]
-    recipes = [re.findall('.+?\.html', recipe)[0] for recipe in recipes]
-    distribution.write("Url рецептов: " + "\n".join(recipes) + "\n")
-    all_recipes.extend(recipes)
-distribution.close()
+            recipes = [recipe['href'] for recipe in recipes]
+            recipes = [
+                recipe if recipe.startswith("https://www.waitrose.com") else 'https://www.waitrose.com' + recipe + ".html"
+                for recipe in recipes]
+            recipes = [re.findall('.+?\.html', recipe)[0] for recipe in recipes]
+            distribution.write("Url рецептов: " + "\n".join(recipes) + "\n")
+            all_recipes.extend(recipes)
+    except ConnectionError:
+        print("Couldn't get " + category)
+
 
 # превращаем полученный url в А4-url
 all_recipes = [recipe[:-5] + ".A4" + recipe[-5:] for recipe in all_recipes]
@@ -48,5 +53,7 @@ all_recipes = set(all_recipes)
 # recipes_urls_*.txt – уникальный список всех url рецептов
 with open(to_file, 'w') as f:
     for recipe_url in all_recipes:
-        f.write(recipe_url + "\n")
-f.close()
+        f.write(recipe_url)
+        if recipe_url is not list(all_recipes)[-1]:
+            f.write("\n")
+
