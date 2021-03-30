9246b137b186f93c9aa31beb12c9f848fe6ab8d0
Valeriia Terova <terovaleriya@gmail.com>
2021-03-30 14:24:34 +0200

    minor changes

--- recipe_parser/get_recipes_json.py
+++ recipe_parser/get_recipes_json.py
@@ -2,17 +2,14 @@ import os
 
 from recipe_parser.parser import get_json
 
-# битые по каким-то причинам html, потом разберусь
-skip = ["perfect-roast-potatoes0.A4.html", "martha-s-pumpkinroastalmondsoup.A4.html",
-        "hundreds-and-thousandscake.A4.html"]
+html_folder = "recipes_html_p"
+json_folder = "recipes_json_p"
 
-if not os.path.exists("recipes_json_A4_plane"):
-    os.makedirs("recipes_json_A4_plane")
+if not os.path.exists(json_folder):
+    os.makedirs(json_folder)
 
-for file in os.listdir("recipes_html_A4"):
-    print(file)
-    if file not in skip:
-        with open("recipes_json_A4_plane/" + file[:-5] + ".json", "w") as f:
-            file = open("recipes_html_A4/" + file, "r")
-            f.write(get_json(file, True))
-            file.close()
+for file in os.listdir(html_folder):
+    with open(json_folder + "/" + file[:-5] + ".json", "w") as f:
+        file = open(html_folder + "/" + file, "r")
+        f.write(get_json(file))
+        file.close()
