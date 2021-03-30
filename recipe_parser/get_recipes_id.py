6ff8addea2f297e359244d0919a94205a32547ae
Valeriia Terova <terovaleriya@gmail.com>
2021-03-30 14:21:33 +0200

    minor changes

--- recipe_parser/get_recipes_id.py
+++ recipe_parser/get_recipes_id.py
@@ -1,12 +1,17 @@
 import re
 
+# файл с url
+url_file = "files/recipes_urls_A4.txt"
+# куда пишем id
+id_file = "files/matches_A4.txt"
+
 # хотим достать уникальные идентефикаторы для рецептов из url (так называются файлы с html рецептов)
-with open('files/recipes_urls_A4.txt', 'r') as urls:
+with open(url_file, 'r') as urls:
     matches = urls.read().split("\n")
     matches = set([re.findall('[^/]+(?=/$|$)', url)[0] for url in matches])
 
 # запишем их в matches_*.txt
-matches_file = open('files/matches_A4.txt', 'w')
+matches_file = open(id_file, 'w')
 for match in matches:
     matches_file.write(match + "\n")
 matches_file.close()
